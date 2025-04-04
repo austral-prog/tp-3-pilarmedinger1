2d959d00fba6e106050d1fc56bd1dfdb3b03f130
pilarmedinger1 <pmedinger@mail.austral.edu.ar>
2025-04-04 18:53:41 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,13 +1,7 @@
 def slice_simple():
     texto = "Awesome"
-
-    primeras_tres = texto[:3]
-    mitad = texto[len(texto)//2 - 1: len(texto)//2 + 2]
-    parte_1 = texto[:4]
-    parte_2 = texto[-3:]
-
-    print(primeras_tres)
-    print(mitad)
-    print(parte_1 + parte_2)
+    print(texto[:3].lower())
+    print(texto[2:5])
+    print(texto.lower())
 
 slice_simple()
