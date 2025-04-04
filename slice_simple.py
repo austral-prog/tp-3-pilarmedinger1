847dafcceb405837383d9ee97645568ecd7c103f
pilarmedinger1 <pmedinger@mail.austral.edu.ar>
2025-04-04 18:41:14 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,8 +1,13 @@
 def slice_simple():
     texto = "Awesome"
-    # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
-    # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
 
+    primeras_tres = texto[:3]
+    mitad = texto[len(texto)//2 - 1: len(texto)//2 + 2]
+    parte_1 = texto[:4]
+    parte_2 = texto[-3:]
 
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
+    print(primeras_tres)
+    print(mitad)
+    print(parte_1 + parte_2)
+
+slice_simple()
